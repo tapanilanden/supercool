92b79bf47682462ee628d62832da2290f966544f
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-28 17:59:17 +0300

    removed migration

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -18,7 +18,8 @@
                     ideas to share their visions with other <em>super cool</em> human beings!
                     </p>
                     <p>Start sharing your SCAAIs by</p>
-                    <a href="#" class="btn btn-primary btn-block">Logging in</a><p class="text-center">or</p>
+                    <a href="#" class="btn btn-primary btn-block">Logging in</a>
+                    <p class="text-center">or</p>
                     <a href="#" class="btn btn-primary btn-block">Create New Account</a>
                 </div>
             
