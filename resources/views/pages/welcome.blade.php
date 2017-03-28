458ba881d476a508048c0cf056a03b1ea45ef9a6
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-28 13:58:18 +0300

    views/partials

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -13,19 +13,21 @@
     </div>
 
     <!-- END OF JUMBOTRON -->
-
+    
+    
+    <div class="row">
     <!-- Starred ideas from INNOVATION WIZARDS -->
 
-    <div class="row">
+    
 
         <div class="col-md-6">
     
-            <h1>Recent SCAAI's</h1>
+            <h1>Starred SCAAI's</h1>
             <p>
             <div class="post">
             <h3>SCAAI</h3>
-            <i>category:</i> <a href="#"><b>innovations</b><a>
-            <i>from:</i> <a href="#"><b>Spede</b><a>
+            <i>category:</i> <a href="#"><b>innovations</b></a>
+            <i>from:</i> <a href="#"><b>Spede</b></a>
             <p> It would be awesome if somebody set me on fire with super some cool stuff aw yiss.</p>
             <a href="#" class="btn btn-primary">Read more</a>
         
@@ -43,8 +45,8 @@
             <p>
             <div class="post">
             <h3>SCAAI</h3>
-            <i>category:</i> <a href="#"><b>innovations</b><a>
-            <i>from:</i> <a href="#"><b>Spede</b><a>
+            <i>category:</i> <a href="#"><b>innovations</b></a>
+            <i>from:</i> <a href="#"><b>Spede</b></a>
             <p> It would be awesome if somebody set me on fire with super some cool stuff aw yiss.</p>
             <a href="#" class="btn btn-primary">Read more</a>
         
