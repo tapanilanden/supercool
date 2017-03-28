66911145edf134b7a3c2569c71f1369a488cc247
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-28 21:00:31 +0300

    view/scaais/show

--- resources/views/scaais/edit.blade.php
+++ resources/views/scaais/edit.blade.php
@@ -2,6 +2,45 @@
 
 @section('content')
 
+    <div class="row">
+        {!! Form::model($scaai, ['route' => ['scaais.update', $scaai->id], 'method' => 'PUT']) !!}
+        <div class="col-md-8">
+            {{ Form::label('title', 'Title:') }}
+            {{ Form::text('title', null, ['class' => 'form-control input-lg']) }}
+            
+            {{ Form::label('user_id','User ID:', ['class', 'form-spacing-top']) }}
+            {{ Form::text('user_id', 'null', ['class' => 'form-control']) }}
+            
+            {{ Form::label('body', 'Body:', ['class' => 'form-spacing-top']) }}
+            {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+        
+        </div>
+        
+        <div class="col-md-4">
+            <div class="well">
+                
+                <dl class="dl-horizontal">
+                    <dt>Created at:</dt>
+                    <dd>{{ date('j.m.Y H:i', strtotime($scaai->created_at)) }}</dd>
+                </dl>
+                
+                <dl class="dl-horizontal">
+                    <dt>Edited at:</dt>
+                    <dd>{{ date('j.m.Y H:i', strtotime($scaai->updated_at)) }}</dd>
+                </dl>
+                <hr>
+                
+                <div class="row">
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('scaais.show', 'Cancel', array($scaai->id), array('class' => 'btn btn-danger btn-block')) !!}                        
+                    </div>
+                    <div class="col-sm-6">
+                        {{ Form::submit('Save Changes', ['class' => 'btn btn-success btn-block']) }}     
+                    </div>
+                </div>
+            </div>
+        </div>
+            {!! Form::close() !!}
+    </div> <!-- end of .row (form) -->
 
-
-@endsection
+@stop
--- resources/views/scaais/index.blade.php
+++ resources/views/scaais/index.blade.php
@@ -2,6 +2,52 @@
 
 @section('content')
 
-
-
-@endsection
+    <div class="row">
+        
+        <div class="col-md-10">
+        
+            <h1>All SCAAIs</h1>
+        
+        </div>
+        
+        <div class="col-md-2">
+            <a href="{{ route('scaais.create') }}" class="btn btn-lg btn-block btn-primary btn-h1-spacing">Innovate New SCAAI</a>
+        </div>
+        <hr>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-12">
+        
+            <table class="table">
+            
+                <thead>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Created At</th>
+                    <th></th>
+                </thead>
+                
+                <tbody>
+                
+                    @foreach ($scaais as $scaai)
+                        <tr>
+                            <th> {{ $scaai->id }} </th>
+                            <td> {{ $scaai->title }} </td>
+                            <td> {{ substr($scaai->body, 0, 50) }}{{ strlen($scaai->body) > 50 ? "..." : "" }} </td>
+                            <td> {{ date('j.m.Y H:i', strtotime($scaai->created_at)) }} </td>
+                            <td> <a href="{{ route('scaais.show', $scaai->id) }}" class="btn btn-default btn-sm">View</a> 
+                                <a href="{{ route('scaais.edit', $scaai->id) }}" class="btn btn-default btn-sm">Edit</a></td>
+                        </tr>     
+                    @endforeach
+                </tbody>  
+            </table>
+        
+            <div class="text-center">
+                {!! $scaais->links(); !!}
+            </div>
+        </div>
+    
+    <div>
+@stop
