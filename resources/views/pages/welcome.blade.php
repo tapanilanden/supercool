0beb4706849882af8c308053ba23cd37e65872d4
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-28 16:07:02 +0300

    views/partials

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('main')
 
-@section('pageheader', '<h1 class="text-right"><small>SUPER COOL AND AWESOME</small>PLATFORM FOR <small>SUPER COOL AND AWESOME </small>IDEAS</h1>')
+@section('pageheader', '<h1 class="text-right"><small>SUPER COOL AND AWESOME</small>PLATFORM FOR <small><br>SUPER COOL AND AWESOME </small>IDEAS</h1>')
 
 @section('content')
 
