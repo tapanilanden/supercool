73c08906eb69aa6bca79cb5bb0e233aa23b2bb5d
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-28 14:04:25 +0300

    views/partials

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -25,15 +25,16 @@
             <h1>Starred SCAAI's</h1>
             <p>
             <div class="post">
-            <h3>SCAAI</h3>
-            <i>category:</i> <a href="#"><b>innovations</b></a>
-            <i>from:</i> <a href="#"><b>Spede</b></a>
-            <p> It would be awesome if somebody set me on fire with super some cool stuff aw yiss.</p>
+                <h3>SCAAI</h3>
+                <p><i>category:</i> <a href="#"><b>innovations</b></a></p>
+                <p><i>from:</i> <a href="#"><b>Spede</b></a></p>
+                <p> It would be awesome if somebody set me on fire with super some cool stuff aw yiss.</p>
             <a href="#" class="btn btn-primary">Read more</a>
-        
-        
+            </div>
+            
     
-        <hr>
+            <hr>
+        </div>
     
     <!-- END OF STARRED IDEAS -->
 
