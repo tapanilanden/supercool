2e00bc85f3328ca2ec5e0f63e0da50c46bdd779e
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-28 12:44:12 +0300

    views

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -67,7 +67,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="jumbotron">
-                        <h1 class="text-justify">SUPER COOL AND AWESOME PLATFORM FOR SUPER COOL AND AWESOME IDEAS</h1>
+                        <h1 class="text-right">SUPER COOL AND AWESOME PLATFORM FOR SUPER COOL AND AWESOME IDEAS</h1>
                     </div>
                 </div>
                 
