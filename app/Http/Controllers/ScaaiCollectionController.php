e79baee368c0457fd3385d53530ffaf251056ecd
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-29 17:09:18 +0300

    auth

--- app/Http/Controllers/ScaaiCollectionController.php
+++ app/Http/Controllers/ScaaiCollectionController.php
@@ -12,7 +12,7 @@ class ScaaiCollectionController extends Controller
 {
      public function index()
     {
-        $scaais = Scaai::->orderBy('id', 'desc')->paginate(5);
+        $scaais = Scaai::orderBy('id', 'desc')->paginate(5);
 
         return view('scaais_collection.index')->withScaais($scaais);
         
