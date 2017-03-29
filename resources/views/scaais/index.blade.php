1f596896ef8202b5bb0d229bce4091c0db31f3e9
Tapani Landén <tapani@Tapani-Mac-mini.local>
2017-03-29 16:14:28 +0300

    auth

--- resources/views/scaais/index.blade.php
+++ resources/views/scaais/index.blade.php
@@ -11,8 +11,8 @@
     <div class="row well-sm">
         <div class="col-md-8 col-md-offset-2">                
             <a href="{{ route('scaais.show', $scaai->id) }}" >
-                <strong>#{{ $scaai->id }} </strong>
-                <h3>{{ $scaai->title }} </h3>
+                <h3><strong>#{{ $scaai->id }} </strong>
+                {{ $scaai->title }} </h3>
                  <p>{{ substr($scaai->body, 0, 50) }}{{ strlen($scaai->body) > 50 ? "..." : "" }} </p>
             </a>   
         </div>
